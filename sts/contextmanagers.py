eaa75a86a3ea64e2c98dbcdd0a0b9731c9505abf
Byron Ruth <b@devel.io>
2013-05-05 00:00:13 -0400

    Add back fail_state to context manager

--- sts/contextmanagers.py
+++ sts/contextmanagers.py
@@ -4,7 +4,7 @@ from .models import System
 class transition(object):
     "Transition context manager."
     def __init__(self, obj, state, event=None, start_time=None,
-            message=None, exception_fail=True):
+            message=None, exception_fail=True, fail_state='Fail'):
 
         self.system = System.get(obj)
         self.transition = self.system.start_transition(event=event,
@@ -12,6 +12,7 @@ class transition(object):
         self.state = state
         self.message = message
         self.exception_fail = exception_fail
+        self.fail_state = fail_state
 
     def __enter__(self):
         return self.transition
@@ -20,10 +21,11 @@ class transition(object):
         if exc_type and self.exception_fail:
             failed = True
         else:
-            failed = None
+            failed = False
 
         # Use the locally set message
         message = self.transition.message or self.message
+        state = self.fail_state if failed else self.state
 
         # End the transition
-        self.system.end_transition(self.state, message=message, failed=failed)
+        self.system.end_transition(state, message=message, failed=failed)
